5a22de9906e065934e4064f8dcea0cc7d94261af
Safwan Ahmad <safwanahmad787@gmail.com>
2024-10-20 20:20:24 +0600

    third

---
+++ Fruit Quiz.py
@@ -0,0 +1,36 @@
+import random
+class FruitQuiz:
+
+
+    def __init__(self):
+
+        self.fruits = {
+            'apple':'red',
+
+            'orange':'orange',
+
+            'watermelon':'green',
+
+            'banana':'yellow'
+        }
+    def quiz(self):
+        while(True):
+
+            fruit, color = random.choice(list(self.fruits.items()))
+
+            print(f"What is the color of {fruit}?")
+            user_answer = input()
+
+            if(user_answer.lower() == color):
+                print("Correct answer")
+
+            else:
+                ("wrong answer")
+
+            option = int(input("Enter 0 if you want to play again, otherwise 1: "))
+            if(option):
+                break
+
+print("Welcome to fruit quiz")
+fq = FruitQuiz()
+fq.quiz()
